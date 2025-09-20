2829ab4f73dcd294f64f1172842bec129ee45d68
kh-parveg-hossain <parevgkh64@gmail.com>
2025-09-21 01:29:10 +0530

    v

--- src/server.js
+++ src/server.js
@@ -26,7 +26,7 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: "http://localhost:" + (process.env.PORT || 4000),
+        url: process.env.BASE_URL + (process.env.PORT || 4000),
       },
     ],
   },
